30b2f9997eb35c9e3f5f8998f249e160f0940ba0
GabrielMelhem <gabrielmelhem@gmail.com>
2023-12-12 16:07:38 +0100

    end styling

--- src/components/DrinkCard/DrinkCard.js
+++ src/components/DrinkCard/DrinkCard.js
@@ -9,20 +9,24 @@ import {
   MDBCardFooter
 } from "mdb-react-ui-kit";
 
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
 import { useState, useEffect } from 'react';
 
 const DrinkCard = (drink) => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+  // const [currentTime, setCurrentTime] = useState(new Date());
+  const [rating, setRating] = useState(0)
 
-  useEffect(() => {
-    // Update the current time every second
-    const interval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
+  // useEffect(() => {
+  //   // Update the current time every second
+  //   const interval = setInterval(() => {
+  //     setCurrentTime(new Date());
+  //   }, 1000);
 
-    // Clean up the interval on component unmount
-    return () => clearInterval(interval);
-  }, []); // Empty dependency array to run this effect only once on mount
+  //   // Clean up the interval on component unmount
+  //   return () => clearInterval(interval);
+  // }, []); // Empty dependency array to run this effect only once on mount
 
 
   return (
@@ -45,7 +49,8 @@ const DrinkCard = (drink) => {
           </MDBCardText>
         </MDBCardBody>
         <MDBCardFooter>
-          <small className="text-muted">Time: {currentTime.toLocaleTimeString()}</small>
+          {/* <small className="text-muted">Time: {currentTime.toLocaleTimeString()}</small> */}
+          <small className="text-muted"><Rating style={{ maxWidth: 130 }} value={rating} onChange={setRating} /></small>
         </MDBCardFooter> 
       </MDBCard>
     </>
